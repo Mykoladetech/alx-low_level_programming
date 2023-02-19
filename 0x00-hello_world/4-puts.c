f7ed66bbd032536d5207c95835f1f26df498dfb9
olayiwola_michael <michaelolayiwola@gmail.com>
2023-02-19 04:32:37 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main print "programming is like building a multilingual puzzle
+ *  and the puzzle is follow by:"
+ *  return 0.
+ */
+
+int main(void);
+{
+	puts("\"proogramming is like building a multilingual puzzle");
+
+		return (0);
+}
