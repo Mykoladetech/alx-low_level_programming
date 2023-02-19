a3fcc8b8058786acfcad29f6c5943007595fee6e
olayiwola_michael <michaelolayiwola@gmail.com>
2023-02-19 05:46:23 -0800

    4-puts.c

---
+++ 0x00-hello_world/dfdfdfdf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *
+ * main = "nigeri"
+ * return 0
+ */
+int main(void)
+{
+	int =age;
+	char =l k j
+	scanf("%c &c %c %c and %d", &l,&k,&j,&age)
+	printf("your favourite %c %c %c of present time: d%\n" l,k,j,age)
+}
