cef151a4d2cf2eccc39f9898336a4fefbf94819d
olayiwola_michael <michaelolayiwola@gmail.com>
2023-02-19 09:58:44 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - "Programming is like building a multilingual puzzle
- * puzzle is underline"
- * return 0
+ * puzzle is underline".
+ * return (0)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle ");
+	return (0);
 }
