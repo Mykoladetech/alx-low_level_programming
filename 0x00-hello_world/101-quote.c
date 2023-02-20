e2e044738e2d8e12e3d809b4447d3cd74708d12c
olayiwola_michael <michaelolayiwola@gmail.com>
2023-02-20 07:56:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include<unistd.h>
 
 /**
- * main - And that price of art is useful -Dora Korpar,2015-10-19
+ * main - print "and that piece of art is useful -Dora Korpar, 2015-10-19",
+ * followed by newline, to standard error.
  * return: always 1 (success)
  */
 int main(void)
 {
-	write(2, "and that price of art is useful\" -Dora Korpar,2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
