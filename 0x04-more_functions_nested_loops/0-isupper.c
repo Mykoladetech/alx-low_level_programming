09e415f7c9a44a63c0b2ccced3e6649cb03a6f73
olayiwola_michael <michaelolayiwola@gmail.com>
2023-02-23 04:16:10 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *	_isupper - function that check for upper case character
+ *	0c:input
+ *	Return: 1 if c is the upper case, 0 otherwise
+ */
+int _isupper(int c)
+{
+	
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
+
+	else
+	
+	return (0);	
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *	print_numbers -  fuction that print digit, from 0 to 9
+ *	Return: 0 to 9, except 3 and 4
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	_putchar(i + '0')
+	_putchar('\n')
+}
