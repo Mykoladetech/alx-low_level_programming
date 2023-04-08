229c8736890f81471c263973afb4ceb7d0efae03
olayiwola_michael <michaelolayiwola@gmail.com>
2023-04-08 11:26:06 -0700

    main.h

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t ear, rint;
+	char *buf_f = malloc(sizeof(char)*letters+1);
+	FILE *file;
+	file = fopen(filename, "O_RDONLY");
+		if(file < 0);
+		return (0);
+	ear = fread (buf_f, sizeof(char), letters, file);
+	rint = fwrite (buf_f, sizeof(char), letters, file);
+	
+
+	free(buf_f);
+	fclose(file);
+	return (0);
+	
+
+
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+/**
+ * append_text_to_file - Appends text to the end of a file.
+ *
+ * @filename: The name of the file to append to.
+ * @text_content: The text to append to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	size_t sen;
+	int k, ead;
+
+	if (filename == NULL)
+	return (-1);
+	if (text_content == NULL)
+	{
+	sen = strlen(text_content);
+	}
+	k = open(filename, O_WRONLY | O_APPEND);
+	if (k == -1)
+	{
+	return (-1);
+	}
+	ead = read(k, text_content, strlen(text_content));
+	if (ead == -1)
+	{	close(k);
+		return (-1);
+	}
+		return (1);
+}
