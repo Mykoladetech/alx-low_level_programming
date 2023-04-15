cfba94923a52f9c01b3e804d45c7f44ab5764a0a
olayiwola_michael <michaelolayiwola@gmail.com>
2023-04-15 16:30:04 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,24 +13,32 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file, len = 0, ead = 0;
+
 	if (filename == NULL)
 	return (-1);
 
-	int len_ss = 0;
-	int ead;
-	int file;
 
 	if (text_content != NULL)
 	{
-		while (*(text_content + len_ss) = '\0')
-		len_ss++;
+		while (*(text_content + len) != '\0')
+		len++;
 
 	file = open(filename, O_WRONLY | O_CREAT | O_APPEND, 00600);
-	ead = write(file, text_content, len_ss);
-	if (file == -1 || ead == -1)
+		if (file == -1)
+	{
 		return (-1);
+	}
+
+	ead = write(file, text_content, len);
+	if (ead == -1)
+	{
+		return (-1);
+
 		close(file);
 	}
+
+	}
 		return (1);
-		close(file);
+
 }
