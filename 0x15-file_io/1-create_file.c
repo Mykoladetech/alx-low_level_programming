f74a81517e5bdfe745a7ca194aae7a15d4670a1e
olayiwola_michael <michaelolayiwola@gmail.com>
2023-04-15 17:00:14 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,23 +20,24 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+
+	if (text_content != NULL)
 	{
-	while (text_content[rint])
+	while (text_content[rint] != '\0')
 		rint++;
-	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file < 0)
+	if (file == -1)
 	{
 	return (-1);
 	}
 	ear = write(file, text_content, rint);
-	if (ear < -1)
+	if (ear == -1)
 	{
 	close(file);
 	return (-1);
 	}
-	close(file);
+
+	}
 	return (1);
 }
