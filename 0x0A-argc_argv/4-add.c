5f6f09069afda2567984ffa68bc4d6b3efe7039a
michael_olayiwola <michaelolayiwola45@gmail.com>
2023-06-09 17:00:42 -0700

    move

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1, sum = 0;
+
+	if (argc < 3 || argc > 3)
+	{
+	printf("error\n");
+	return (1);
+	}
+	while (i <= 2)
+	{
+	sum += atoi(argv[i]);
+	i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
