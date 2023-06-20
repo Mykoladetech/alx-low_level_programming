e519b053e5f3488be57826bee53a2482537bbb3a
michael_olayiwola <michaelolayiwola45@gmail.com>
2023-06-20 02:30:57 -0700

    we_move

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * mat - Prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int mat(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
