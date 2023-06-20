862c59d969000f0851fe5fcc87aba0236b5df37f
michael_olayiwola <michaelolayiwola45@gmail.com>
2023-06-20 04:30:14 -0700

    never_give_up

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,8 +1,6 @@
-#include <stdio.h>
 #ifndef SUM_H
 #define SUM_H
 
 #define SUM(x, y) ((x) + (y))
 
 #endif
-
